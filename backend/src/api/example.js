ea11cdae5afd9c893828ab6c32edc0e6753a84db
Mathias H. Weber <webmathias@gmail.com>
2018-06-27 20:55:20 -0300

    Example get without restify to populate examples in database

--- backend/src/api/example.js
+++ backend/src/api/example.js
@@ -22,5 +22,15 @@ restify.serve(
  * PATCH http://<ip>/api/v1/example/:id
  * DELETE http://<ip>/api/v1/example/:id
  * 
- **/ 
+ **/
+router.get('/v1/populate', (req, res) => {
+    const { qtd = 50 } = req.query
+    const Model = require('./db/example');
+    for (let i = 0; i < qtd; i++) {
+        let inst = new Model({name:'Example', type:''+i});
+        inst.save();
+    }
+    res.send('Populating examples')
+
+})
 module.exports = router
